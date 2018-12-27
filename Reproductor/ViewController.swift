8b9eddc4e3e0967bbc46c623c22235597dabecdc
Pablo <pablo@Pablos-iMac.local>
2018-12-27 08:13:12 -0800

    Initial Commit

---
+++ Reproductor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reproductor
+//
+//  Created by Pablo on 27/12/2018.
+//  Copyright © 2018 Pablo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
